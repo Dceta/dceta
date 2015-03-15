170bcc53be43d96d0c8b510b77cdce8dd006c3b9
denysboiko <denys.bojko@gmail.com>
2015-03-15 20:33:02 +0200

    logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,14 @@
     // CAROUSEL PREVIEW
 
     (function(){
-        var next=$("#dceta-slider .item").eq(1).text();
-        var prev=$("#dceta-slider .item").eq($(this).length+1).text();
+        var next = $("#dceta-slider .item").eq(1).find(".carousel-caption").text();
+        var prev = $("#dceta-slider .item ").eq($(this).length + 1).text();
 
         $('#dceta-slider').on('slid.bs.carousel', function () {
-            next=$("#dceta-slider .item.active").next(".item").find(".carousel-caption").html();
+            next = $("#dceta-slider .item.active").next(".item").find(".carousel-caption").html();
             if (typeof next == "undefined") next=$("#dceta-slider .item").eq(0).text();
 
-            prev=$("#dceta-slider .item.active").prev(".item").find(".carousel-caption").html();
+            prev = $("#dceta-slider .item.active").prev(".item").find(".carousel-caption").html();
             if (typeof prev == "undefined") prev=$("#dceta-slider .item").eq($(this).length+1).text();
             $(".carousel-control.left div h3").html(prev);
             $(".carousel-control.right div h3").html(next);
