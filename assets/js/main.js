deb39fca37c25d70c9ac8e59df4cb5c8fc6a109a
denysboiko <denys.bojko@gmail.com>
2015-03-15 23:18:33 +0200

    slider-fixes, modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,15 @@
     // CAROUSEL PREVIEW
 
     (function(){
-        var next = $("#dceta-slider .item").eq(1).find(".carousel-caption").text();
-        var prev = $("#dceta-slider .item ").eq($(this).length + 1).text();
+        var next = $("#dceta-slider .item").eq(1).find("h2").text();
+        var prev = $("#dceta-slider .item ").eq($(this).length + 1).find("h2").text();
 
         $('#dceta-slider').on('slid.bs.carousel', function () {
-            next = $("#dceta-slider .item.active").next(".item").find(".carousel-caption").html();
-            if (typeof next == "undefined") next=$("#dceta-slider .item").eq(0).text();
+            next = $("#dceta-slider .item.active").next(".item").find("h2").html();
+            if (typeof next == "undefined") next=$("#dceta-slider .item").eq(0).find("h2").text();
 
-            prev = $("#dceta-slider .item.active").prev(".item").find(".carousel-caption").html();
-            if (typeof prev == "undefined") prev=$("#dceta-slider .item").eq($(this).length+1).text();
+            prev = $("#dceta-slider .item.active").prev(".item").find("h2").html();
+            if (typeof prev == "undefined") prev=$("#dceta-slider .item").eq($(this).length+1).find("h2").text();
             $(".carousel-control.left div h3").html(prev);
             $(".carousel-control.right div h3").html(next);
         });
