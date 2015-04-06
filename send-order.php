0a588354db671f677e3a7484a5cfc771893826a6
denysboiko <denys.bojko@gmail.com>
2015-04-06 17:33:38 +0300

    removed assets

---
+++ send-order.php
@@ -0,0 +1,13 @@
+<?php
+
+    /*$name = $_POST["name"]
+    $number = $_POST["message"]*/
+    $name="Denys";
+    $name="3495i";
+    $to = 'dceta@email.com';
+    $subject = 'Order';
+    $message = "Имя: " . $name . "\n" . "Телефон: " . $number;
+    $headers = 'From: Dceta Studio <support@dceta.com/>' . "\r\n";
+ 
+    //mail($to, $subject, $message, $headers);
+?>
